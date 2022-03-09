056eef396d46db287544a64ff2326cf3bca38428
Alienware <Alienware@Swap>
2022-03-10 01:47:29 +0530

    Spring Security Dependency Added

--- smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -1,5 +1,14 @@
 package com.smart.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/user")
 public class UserController {
 
+	@RequestMapping(value = "/index")
+	public String dashboard() {
+		return "normal/user_dashboard";
+	}
 }
